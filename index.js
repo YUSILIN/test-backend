3fc83b91f6d3a52ae60cdc36660f0fe9ce7ea886
YusVelasquez <yuselinvelasquez@gmail.com>
2022-07-21 21:58:52 -0500

    tercer cambio

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require('express')
 const app=express()
 
 app.listen(3000, () =>{
     console.log('Mi servidor empezo')
+    console.log(process.env.PUERTO)
 })
+
